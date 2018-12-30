9c17ce535f1c62fd30c9373a07c7c617a980ab02
CitrixQian <sijieqian@foxmail.com>
2018-12-30 21:26:35 +0800

    第四题

---
+++ src/chap2/Question_2.java
@@ -0,0 +1,7 @@
+package chap2;
+
+/**
+ * Created by Citrix on 2018-12-23.
+ */
+public class Question_2 {
+}
---
+++ src/chap2/Question_4.java
@@ -0,0 +1,30 @@
+package chap2;
+
+
+/**
+ * Created by Citrix on 2018-12-30.
+ */
+/*
+ * 思路马上想到了，之前在LeetCode上做过
+ * */
+public class Question_4 {
+    public boolean Find(int target, int[][] array) {
+        if (array.length == 0) {
+            return false;
+        }
+        int m = array.length;
+        int n = array[0].length;
+        int i = 0;
+        int j = n - 1;
+        while (i < m && j >= 0) {
+            if (array[i][j] > target) {
+                j--;
+            } else if (array[i][j] < target) {
+                i++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
