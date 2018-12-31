6ac34fc9b5e9164c9355e701c7e31ab1bbb8367c
CitrixQian <sijieqian@foxmail.com>
2018-12-31 15:14:42 +0800

    第6题second版

--- src/chap2/Question_6.java
+++ src/chap2/Question_6.java
@@ -4,16 +4,18 @@ import structure.ListNode;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Stack;
 
 /**
  * Created by Citrix on 2018-12-31.
  */
-/*
- * 最快想到的方法是先从头到尾打印，然后反转即可（链表的特点是逆行很麻烦）
- * */
 public class Question_6 {
+    /*
+     * 思路一：最快想到的方法是先从头到尾打印，然后反转即可（链表的特点是逆行很麻烦）
+     * 思路二：用堆栈的方式到达逆序
+     * */
     public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
-        ArrayList<Integer> res = new ArrayList<>();
+           /* ArrayList<Integer> res = new ArrayList<>();
         if (listNode == null) {
             return res;
         }
@@ -22,6 +24,19 @@ public class Question_6 {
             listNode = listNode.next;
         }
         Collections.reverse(res);
+        return res;*/
+        ArrayList<Integer> res = new ArrayList<>();
+        if (listNode == null) {
+            return res;
+        }
+        Stack<Integer> stack = new Stack<>();
+        while (listNode != null) {
+            stack.push(listNode.val);
+            listNode = listNode.next;
+        }
+        while (!stack.empty()) {//判断堆栈为空要用stack.empty()而不是stack.pop()==null
+            res.add(stack.pop());
+        }
         return res;
     }
 }
