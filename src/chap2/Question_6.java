b540d205d061345147ea2131ec9b28f646cd56ad
CitrixQian <sijieqian@foxmail.com>
2018-12-31 15:06:56 +0800

    第6题first版

--- src/chap2/Question_6.java
+++ src/chap2/Question_6.java
@@ -1,7 +1,27 @@
 package chap2;
 
+import structure.ListNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by Citrix on 2018-12-31.
  */
+/*
+ * 最快想到的方法是先从头到尾打印，然后反转即可（链表的特点是逆行很麻烦）
+ * */
 public class Question_6 {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (listNode == null) {
+            return res;
+        }
+        while (listNode != null) {
+            res.add(listNode.val);
+            listNode = listNode.next;
+        }
+        Collections.reverse(res);
+        return res;
+    }
 }
