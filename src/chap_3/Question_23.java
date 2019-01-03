0fc73a1c141da4052ce2acb905ac1860ec1499e6
CitrixQian <sijieqian@foxmail.com>
2019-01-03 21:41:23 +0800

    第23题-winner版

--- src/chap_3/Question_23.java
+++ src/chap_3/Question_23.java
@@ -1,8 +1,29 @@
 package chap_3;
 
+import structure.ListNode;
+
 /**
  * Created by Citrix on 2019-01-03.
  */
 public class Question_23 {
-
+    public ListNode EntryNodeOfLoop(ListNode pHead) {
+        if (pHead == null) {
+            return null;
+        }
+        ListNode fastNode = pHead;
+        ListNode slowNode = pHead;
+        while (fastNode != null && fastNode.next != null) {
+            fastNode = fastNode.next.next;
+            slowNode = slowNode.next;
+            if (fastNode == slowNode) {
+                ListNode slowNode2 = pHead;
+                while (slowNode2 != slowNode) {
+                    slowNode = slowNode.next;
+                    slowNode2 = slowNode2.next;
+                }
+                return slowNode;
+            }
+        }
+        return null;
+    }
 }
