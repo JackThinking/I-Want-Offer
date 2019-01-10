515bb0e805ccaae709790e9c9f6450ef5f1f8e61
CitrixQian <sijieqian@foxmail.com>
2019-01-10 20:30:13 +0800

    第32-plus题-loser版

---
+++ src/chap2/Question_14.java
@@ -0,0 +1,8 @@
+package chap2;
+
+/**
+ * Created by Citrix on 2019-01-10.
+ */
+public class Question_14 {
+
+}
--- src/chap_4/Question_32_plus.java
+++ src/chap_4/Question_32_plus.java
@@ -1,7 +1,33 @@
 package chap_4;
 
+import structure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * Created by Citrix on 2019-01-09.
  */
 public class Question_32_plus {
+    ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if (pRoot == null) {
+            return res;
+        }
+        helper(res, pRoot, 0);
+        return res;
+    }
+
+    private void helper(ArrayList<ArrayList<Integer>> res, TreeNode root, int level) {
+        if (root == null) {
+            return;
+        }
+        if (res.size() <= level) {
+            res.add(new ArrayList<>());
+        }
+
+        res.get(level).add(root.val);
+        helper(res, root.left, level + 1);
+        helper(res, root.right, level + 1);
+    }
 }
