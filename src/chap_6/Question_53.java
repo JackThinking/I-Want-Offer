fd133f7fdf3d1914f6416e52c490ce42141e6ea9
CitrixQian <sijieqian@foxmail.com>
2019-01-11 15:33:48 +0800

    第53题-loser版

---
+++ src/chap_6/Question_53.java
@@ -0,0 +1,62 @@
+package chap_6;
+
+/**
+ * Created by Citrix on 2019-01-11.
+ */
+public class Question_53 {
+    /*
+     * 先二叉找到，然后左右平滑计数
+     * */
+    public int GetNumberOfK(int[] array, int k) {
+
+        if (array.length != 0) {
+            int left = getFirstK(array, k);
+            int right = getLastK(array, k);
+            if (left != -1 && right != -1) {
+                return right - left + 1;
+            }
+        }
+
+        return 0;
+    }
+
+    private int getFirstK(int[] array, int k) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {//一般还是等于的多
+            int mid = left + (right - left) / 2;
+            if (array[mid] == k) {
+                if (mid > 0 && array[mid - 1] == k) {
+                    right = mid - 1;
+                } else {
+                    return mid;
+                }
+            } else if (array[mid] > k) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    private int getLastK(int[] array, int k) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {//一般还是等于的多
+            int mid = left + (right - left) / 2;
+            if (array[mid] == k) {
+                if (mid < array.length - 1 && array[mid + 1] == k) {
+                    left = mid + 1;
+                } else {
+                    return mid;
+                }
+            } else if (array[mid] > k) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
