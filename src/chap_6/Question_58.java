3879fceaaf28e8e7a1a22ebd0490bd0f666142b4
CitrixQian <sijieqian@foxmail.com>
2019-01-14 22:12:02 +0800

    第58题-winner版

---
+++ src/chap_6/Question_58.java
@@ -0,0 +1,47 @@
+package chap_6;
+
+import chap2.Question_4;
+
+/**
+ * Created by Citrix on 2019-01-14.
+ */
+public class Question_58 {
+    public String ReverseSentence(String str) {
+        char[] chars = str.toCharArray();
+        swap(chars, 0, chars.length - 1);
+        int start = 0;
+        int end = 0;
+        while (start < chars.length) {
+            if (chars[start] == ' ') {//这点也没有想到
+                start++;
+                end++;
+            } else if (end == chars.length ||chars[end] == ' ') {//这点没有想到
+                swap(chars, start, end - 1);
+                end++;
+                start = end;
+            } else {
+                end++;
+            }
+        }
+        return new String(chars);
+    }
+
+    private void swap(char[] chars, int start, int end) {
+        if (chars == null || chars.length < 1 || start < 0 || end > chars.length || start > end) {
+            return;
+        }
+        while (start < end) {
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Question_58 q = new Question_58();
+        String str = "I am a student.";
+        System.out.println(q.ReverseSentence(str));
+    }
+}
