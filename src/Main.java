b9ac1cb9f7f08fa372b2b43269bc359ab5fe2c15
CitrixQian <851550494@qq.com>
2019-09-01 14:50:11 +0800

    ok

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,6 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        String str = input.readLine();
-        String[] inputLine = str.split(" ");
-        int n = Integer.parseInt(inputLine[0]);
-        String[] array = input.readLine().split("");
-        int[] arrayInt = new int[array.length];
-        for (int i = 0; i < n; i++) {
-            arrayInt[i] = Integer.parseInt(array[i]);
-        }
+
     }
 }
