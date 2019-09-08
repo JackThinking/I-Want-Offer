e577fe57c6b619ed04cc28661555457d403b46b9
CitrixQian <851550494@qq.com>
2019-09-08 16:19:24 +0800

    ok

--- src/Main.java
+++ src/Main.java
@@ -2,63 +2,66 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        String[] num = input.readLine().split(" ");
-        int T = Integer.parseInt(num[0]);
-        for (int i = 0; i < T; i++) {
-            String[] pars = input.readLine().split(" ");
-            int m = Integer.parseInt(pars[0]);
-            int n = Integer.parseInt(pars[1]);
-            List<Integer> list = new ArrayList<>();
-            if (n != 0) {
-                String[] inputString = input.readLine().split(" ");
-                for (int j = 0; j < inputString.length; j++) {
-                    list.add(Integer.parseInt(inputString[j]));
-                }
-            } else {
-                input.readLine();
-            }
-            System.out.println(solution(list, m, n));
+        int n = Integer.parseInt(input.readLine());
+        String[] inputString = input.readLine().split(" ");
+        List<Integer> inputInt = new ArrayList<>();
+        for (int i = 0; i < inputString.length; i++) {
+            inputInt.add(Integer.parseInt(inputString[i]));
         }
+        System.out.println(solution(inputInt, n));
     }
 
-    private static int solution(List<Integer> list, int m, int n) {
-        int[] month = new int[31];
-        // 赋初始值
-        if (m == 0) {
-            return 30;
+    private static int solution(List<Integer> inputInt, int n) {
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = i + 1;
         }
-        if (!list.isEmpty()) {
-            for (Integer day : list) {
-                month[day] = 1;
-            }
-        }
-        for (int i = 1; i <= 30; i++) {
-            boolean flag = true;
-            for (int j = 1; j <= m; j++) {
-                if ((i + j > 30 || month[i + j] == 0) && ((i - j < 1 || month[i - j] == 0))) {
-                    continue;
+        List<List<Integer>> res = permute(nums);
+        int result = 0;
+        for (List<Integer> list : res) {
+            List<Integer> tmp = new ArrayList<>();
+            for (int i = 1; i < list.size(); i++) {
+                if (list.get(i) - list.get(i - 1) < 0) {
+                    tmp.add(1);
                 } else {
-                    flag = false;
+                    tmp.add(0);
+                }
+                if (tmp.equals(inputInt)) {
+                    result++;
                 }
             }
-            if (flag == true) {
-                month[i] = 1;
-            }
         }
-        // System.out.println(Arrays.toString(month));
-        // 计算答案
-        int res = 0;
-        for (int i = 1; i <= 30; i++) {
-            if (month[i] == 1) {
-                res++;
-            }
-        }
+        return result % 1000000007;
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List list = new ArrayList();
+        boolean[] visited = new boolean[nums.length];
+        dfs(nums, visited, list, res);
         return res;
     }
 
+    private static void dfs(int[] nums, boolean[] visited, List list, List<List<Integer>> res) {
+        if (list.size() == nums.length) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i] == false) {
+                visited[i] = true;
+                list.add(nums[i]);
+                dfs(nums, visited, list, res);
+                visited[i] = false;
+                list.remove(list.size() - 1);
+            }
+        }
+    }
 }
