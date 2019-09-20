73d7c113576f83d77ec0caeba2f74085ae8b1172
JackThinking <851550494@qq.com>
2019-09-20 19:11:21 +0800

    ok

---
+++ src/playground/concurrent/Task.java
@@ -0,0 +1,12 @@
+package playground.concurrent;
+
+/**
+ * Created by Citrix on 2019/9/18.
+ */
+public class Task {
+    public int no;
+
+    public Task(int no) {
+        this.no = no;
+    }
+}
