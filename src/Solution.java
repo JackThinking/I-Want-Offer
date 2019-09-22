7827be4930760c719badd9452a481f4035051852
JackThinking <851550494@qq.com>
2019-09-22 09:25:30 +0800

    ok

--- src/Solution.java
+++ src/Solution.java
@@ -1,43 +1,59 @@
-public class Solution {
-    int count = 0;
-    public int InversePairs(int [] array) {
-        if(array.length==0){
-            return 0;
-        }
-        mergeSort(array,0,array.length-1);
-        return count;
-    }
-    private void mergeSort(int [] array, int left,int right){
-        if(left>=right){
-            return;
-        }
-        int mid = left + (right-left)/2;
-        mergeSort(array,left,mid);
-        mergeSort(array,mid+1,right);
-        merge(array,left,mid,right);
-    }
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
-    private void merge(int [] array, int left,int mid, int right){
-        int[] tmp = new int[right - left + 1];//point
-        int i = left, j = mid + 1, k = 0;//point必须要把i,j,k定义在外面
-        while (i <= mid && j <= right) {//point
-            if (array[i] <= array[j]) {
-                tmp[k++] = array[i++];
-            } else {
-                tmp[k++] = array[j++];
-                count += mid - i + 1;//core point
-                count%=1000000007;
+public class Solution {
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        // 先处理极端情况
+        if (numCourses <= 0) {
+            return new int[0];
+        }
+        // 邻接表表示
+        HashSet<Integer>[] graph = new HashSet[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new HashSet<>();
+        }
+        // 入度表
+        int[] inDegree = new int[numCourses];
+        // 遍历 prerequisites 的时候，把 邻接表 和 入度表 都填上
+        for (int[] p : prerequisites) {
+            graph[p[1]].add(p[0]);
+            inDegree[p[0]]++;
+        }
+        LinkedList<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                queue.addLast(i);
             }
         }
-        while (i <= mid) {
-            tmp[k++] = array[i++];
+        ArrayList<Integer> res = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            // 当前入度为 0 的结点
+            Integer inDegreeNode = queue.removeFirst();
+            // 加入结果集中
+            res.add(inDegreeNode);
+            // 下面从图中删去
+            // 得到所有的后继课程，接下来把它们的入度全部减去 1
+            HashSet<Integer> nextCourses = graph[inDegreeNode];
+            for (Integer nextCourse : nextCourses) {
+                inDegree[nextCourse]--;
+                // 马上检测该结点的入度是否为 0，如果为 0，马上加入队列
+                if (inDegree[nextCourse] == 0) {
+                    queue.addLast(nextCourse);
+                }
+            }
         }
-        while (j <= right) {
-            tmp[k++] = array[j++];
-        }
-        for (k = 0; k < tmp.length; k++) {
-            array[left + k] = tmp[k];
+        // 如果结果集中的数量不等于结点的数量，就不能完成课程任务，这一点是拓扑排序的结论
+        int resLen = res.size();
+        if (resLen == numCourses) {
+            int[] ret = new int[numCourses];
+            for (int i = 0; i < numCourses; i++) {
+                ret[i] = res.get(i);
+            }
+            return ret;
+        } else {
+            return new int[0];
         }
     }
-
 }
